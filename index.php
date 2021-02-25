1052c056ba3b395a896ee5dee8b1442571792a31
Matthew Vallance <matthew.vallance@outlook.com>
2021-02-25 16:04:05 +0000

    First start - Maze game

--- index.php
+++ index.php
@@ -1,16 +1,51 @@
 <html>
-    <header>
+    <head>
         <title>Maze game</title>
-        <link rel="stylesheet" href="resources/style.css">
         <link rel="stylesheet" href="resources/maze.css">
-    </header>
-    <?php
-        $content = "";
+        <link rel="stylesheet" href="resources/style.css">
+        <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.1.js"></script>
+    </head>
+        <body>
+            <?php
+                // Set up lauout variables
+                $content = '<canvas id="game_board" width="1200px" height="1200px"></canvas>';
 
+                // Create maze layout
+                $grid_size = 40;
+                $grid_object = [];
 
-        echo($content);
-    ?>
-    <footer>
-    
-    </footer>
+                for($y = 0; $y <= $grid_size; $y++) {
+                    
+                    $row_array = [];
+                    for($x = 0; $x <= $grid_size; $x++) {
+                        $block = rand(0, 1);
+                        array_push($row_array, $block);
+                    }
+
+                    // Put the finish line somewhere in the final row
+                    if($y == 40) {
+                        $finish_block = rand(1, 40);
+                        $row_array[$finish_block] = -1;
+                    } 
+
+                    $grid_object[$y] = $row_array;
+                }
+                // Make object avaliable in maze.js
+                $content .= '<script type="text/javascript">
+                                var grid_object = '. json_encode($grid_object) . '; 
+                            </script>';
+
+                $script = '<script type="text/javascript" src="maze.js"></script>';
+
+                // Put the content on the page
+                $content .= $script;
+                echo($content);
+
+            ?>
+        </body>
+    <footer></footer>
 </html>
+
+<?php
+
+?>
