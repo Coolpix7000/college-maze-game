0fe6ff35670461ab95001d9b19f5a2ecd6a2fc52
Matthew Vallance <matthew.vallance@outlook.com>
2021-02-25 11:10:54 +0000

    Initial commit - Basic organisation

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <header>
+        <title>Maze game</title>
+        <link rel="stylesheet" href="resources/style.css">
+        <link rel="stylesheet" href="resources/maze.css">
+    </header>
+    <?php
+        $content = "";
+
+
+        echo($content);
+    ?>
+    <footer>
+    
+    </footer>
+</html>
